9fbb883f01720bb963592d1bc3e65fdccbe7b0f6
Tchamdja Milca <milcatchamdja@yahoo.com>
2025-11-20 13:37:06 -0500

    Modification .env_pythonanywhere

--- Flask_app.py
+++ Flask_app.py
@@ -6,18 +6,23 @@ from flask import Flask, render_template, redirect
 from compte import bp_compte
 from mur import bp_mur
 import bd
+import os 
+import dotenv
+
 
 
 app = Flask(__name__)
+
+if not os.getenv('BD_UTILISATEUR'):
+    dotenv.load_dotenv('.env')
 
 app.logger.setLevel(logging.DEBUG)
 app.register_blueprint(bp_mur, url_prefix='/mur')
 app.register_blueprint(bp_compte,url_prefix='/compte')
 
 # Faire la commande suivante pour générer une chaîne aléatoire :
-# python -c "import secrets; print(secrets.token_hex())"
-app.secret_key = "Cette chaîne servira pour l'encryption de la session. \
-                  Elle doit être générée aléatoirement"
+# python -c "import secrets; print(secrets.token_hex())
+app.secret_key =os.getenv('SECRET_SESSION') 
 
 
 @app.route('/')
@@ -32,6 +37,6 @@ def index():
 def non_autorise(e):
     app.logger.exception(e)
     return redirect('/')
-
+"
 if __name__ == '__main__':
     app.run(debug=True)
